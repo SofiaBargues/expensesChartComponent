07d0a94cc7593b1537f36dc0ea43336d80d2bdf6
Sofia Bargues <barguessofia@gmail.com>
2024-06-14 15:58:34 +0100

    something

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,14 @@ import { useState } from "react";
 function App() {
   const [daySelected, setDaySelected] = useState("");
   const [dayHovered, setDayHovered] = useState("");
+  const [maxAmount, setMaxAmount] = useState(0);
 
   const handleMouseLeave = () => {
     setDayHovered("");
   };
+
   console.log(dayHovered);
+
   return (
     <div className="bg-[#f7e9dc] h-screen flex flex-col justify-center">
       <div className="m-auto flex gap-[24px] flex-col">
@@ -26,26 +29,26 @@ function App() {
           <div className="text-[#321b10] text-4xl font-bold">
             Spending - Last 7 days
           </div>
-          <div className="flex flex-row m-auto gap-2 items-end ">
+          <div className="flex flex-row gap-2 items-end h-[250px] w-full justify-between ">
             {data.map((daySpending, index) => (
-              <div className=" flex flex-col justify-between ">
+              <div className=" flex flex-col justify-end h-full ">
                 {daySpending.day === daySelected ||
                 dayHovered === daySpending.day ? (
                   <div className="bg-[#392313] w-14 text-center text-white text-xs p-1 mb-2 rounded rounded-xs ">
                     $ {daySpending.amount}
                   </div>
                 ) : null}
-                <div className="flex flex-col justify-center">
+                <div className="flex flex-col justify-end  h-[80%] items-center">
                   <button
                     onClick={() => setDaySelected(daySpending.day)}
                     onMouseEnter={() => setDayHovered(daySpending.day)}
                     onMouseLeave={handleMouseLeave}
-                    className={`w-10 rounded-md  ${
+                    className={`w-10 rounded-md   ${
                       daySelected === daySpending.day
                         ? "hover:bg-[#9cc7cc] bg-[#76979b]"
                         : "bg-[#ec755d] hover:bg-[#ff9b87]"
                     } `}
-                    style={{ height: daySpending.amount * 3.5 }}
+                    style={{ height: `${100 * (daySpending.amount / 52.36)}%` }}
                   ></button>
                 </div>
                 <div className="flex justify-center">{daySpending.day}</div>
@@ -53,11 +56,15 @@ function App() {
             ))}
           </div>
           <hr></hr>
-          <div>
-            <div>Total this month</div>
-            <div>$478.33</div>
+          <div className="flex flex-row justify-between ">
             <div>
-              <div>+2.4%</div>
+              <div>Total this month</div>
+              <div className="text-[#321b10] text-4xl font-bold">$478.33</div>
+            </div>
+            <div>
+              <div className="text-[#321b10] text-lg font-bold text-end">
+                +2.4%
+              </div>
               <div>from last month</div>
             </div>
           </div>
