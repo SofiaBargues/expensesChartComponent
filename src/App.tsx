23a24d9ad2ced89cf9f55b6820c8c639758dfcd4
Sofia Bargues <barguessofia@gmail.com>
2024-06-14 03:27:07 +0100

    allgo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 // import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
+import data from "./data.json";
 import logo from "./assets/logo.svg";
+import { useState } from "react";
 // import viteLogo from "/vite.svg";
 
 function App() {
-  // const [count, setCount] = useState(0);
+  const [day, setDay] = useState("");
 
+  const selectedDay = (selectedDay) => {
+    setDay(selectedDay);
+  };
+  console.log(day);
   return (
     <div className="bg-[#f7e9dc] h-screen flex flex-col justify-center">
       <div className="m-auto flex gap-[24px] flex-col">
@@ -20,13 +25,31 @@ function App() {
           <div className="text-[#321b10] text-4xl font-bold">
             Spending - Last 7 days
           </div>
-          <div>grafico</div>
+          <div className="flex flex-row m-auto gap-7 items-end">
+            {/* map loquito  */}
+            {data.map((currentDay, index) => (
+              <div>
+                <button
+                  onClick={() => selectedDay(currentDay.day)}
+                  className={`w-10 ${
+                    day === currentDay.day
+                      ? "bg-[#9cc7cc] hover:bg-[#76979b]"
+                      : "bg-[#ec755d] hover:bg-[#ff9b87]"
+                  } `}
+                  style={{ height: currentDay.amount * 4 }}
+                ></button>
+                <div className="flex justify-center">{currentDay.day}</div>
+              </div>
+            ))}
+          </div>
           <hr></hr>
-          <div flex>
-            <div>total this month</div>
-            <div>+2.4%</div>
+          <div>
+            <div>Total this month</div>
             <div>$478.33</div>
-            <div>from last month</div>
+            <div>
+              <div>+2.4%</div>
+              <div>from last month</div>
+            </div>
           </div>
         </div>
       </div>
